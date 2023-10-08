969f4ac3660d214f55f186e8b0030a62f243ab19
MinjaeLee <2alswo7@khu.ac.kr>
2023-10-08 12:18:09 +0900

    [Add] #1 - 프로젝트 파일 추가

---
+++ sopt33-assignment-Weather/sopt33-assignment-Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sopt33-assignment-Weather
+//
+//  Created by 민 on 10/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
