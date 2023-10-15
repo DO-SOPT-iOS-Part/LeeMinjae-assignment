9575ea08995149c97c5113ff4e3120638456e465
MinjaeLee <2alswo7@khu.ac.kr>
2023-10-15 11:38:33 +0900

    [Add] #2 - Icon Asset 추가하기

---
+++ iOS-Assignment-Weather-Clone/iOS-Assignment-Weather-Clone/Resources/Constants/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  iOS-Assignment-Weather-Clone
+//
+//  Created by 민 on 10/15/23.
+//
+
+import Foundation
+
+extension Const {
+    struct ViewController {
+        
+        struct Identifier {
+            static let viewController = "ViewController"
+        }
+    }
+}
