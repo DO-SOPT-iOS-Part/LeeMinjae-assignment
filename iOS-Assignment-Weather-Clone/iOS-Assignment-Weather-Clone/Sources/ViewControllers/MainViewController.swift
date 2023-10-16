0b573820abc1a7265346808eec96651cc2a9a7d8
MinjaeLee <2alswo7@khu.ac.kr>
2023-10-16 15:44:51 +0900

    [Feat] #2 - 메인뷰 레이아웃 구현 완 & Then, SnapKit 사용

--- iOS-Assignment-Weather-Clone/iOS-Assignment-Weather-Clone/Sources/ViewControllers/MainViewController.swift
+++ iOS-Assignment-Weather-Clone/iOS-Assignment-Weather-Clone/Sources/ViewControllers/MainViewController.swift
@@ -6,73 +6,50 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
-class MainViewController: UIViewController {
+final class MainViewController: UIViewController {
     
     // MARK: - UI Components
     private let searchController = UISearchController()
-    
-    private let mainScrollView: UIScrollView = {
-        let scrollView = UIScrollView()
-        scrollView.alwaysBounceVertical = true
-        return scrollView
-    }()
-    
     private let mainContentView = UIView()
-    
-    private let locationStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.distribution = .fillEqually
-        stackView.spacing = 15
-        return stackView
-    }()
-    
-    private let locationView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = #imageLiteral(resourceName: "listCloudImage")
-        return imageView
-    }()
-    
-    private let myLocationLabel: UILabel = {
-        let label = UILabel()
-        label.text = "My Location"
-        label.textColor = .white
-        label.font = UIFont(name: "SFPro-Heavy", size: 25)
-        return label
-    }()
-    
-    private let cityLabel: UILabel = {
-        let label = UILabel()
-        label.text = "천안시"
-        label.textColor = .white
-        
-        return label
-    }()
-    
-    private let weatherLabel: UILabel = {
-        let label = UILabel()
-        label.text = "흐림"
-        label.textColor = .white
-        
-        return label
-    }()
-    
-    private let tempLabel: UILabel = {
-        let label = UILabel()
-        label.text = "21°"
-        label.textColor = .white
-        
-        return label
-    }()
-    
-    private let maxMinTempLabel: UILabel = {
-        let label = UILabel()
-        label.text = "최고:29° 최저:29°"
-        label.textColor = .white
-        
-        return label
-    }()
+    private let mainScrollView = UIScrollView().then() {
+        $0.alwaysBounceVertical = true
+    }
+    private let locationStackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.distribution = .fillEqually
+        $0.spacing = 15
+    }
+    private let locationView = UIImageView().then {
+        $0.image = #imageLiteral(resourceName: "listCloudImage")
+    }
+    private let myLocationLabel = UILabel().then {
+        $0.text = "My Location"
+        $0.textColor = .white
+        $0.font = UIFont(name: "SFPro-Heavy", size: 25)
+    }
+    private let cityLabel = UILabel().then {
+        $0.text = "천안시"
+        $0.textColor = .white
+        $0.font = UIFont(name: "SFPro-Light", size: 16)
+    }
+    private let weatherLabel = UILabel().then {
+        $0.text = "흐림"
+        $0.textColor = .white
+        $0.font = UIFont(name: "SFPro-Light", size: 16)
+    }
+    private let tempLabel = UILabel().then {
+        $0.text = "21°"
+        $0.textColor = .white
+        $0.font = UIFont(name: "SFPro-Medium", size: 53)
+    }
+    private let maxMinTempLabel = UILabel().then {
+        $0.text = "최고:29° 최저:29°"
+        $0.textColor = .white
+        $0.font = UIFont(name: "SFPro-Light", size: 15)
+    }
     
     // MARK: - View Life Cycle
     override func viewDidLoad() {
@@ -93,13 +70,6 @@ class MainViewController: UIViewController {
 extension MainViewController {
     // UI 세팅
     private func setupUI() {
-        self.view.addSubview(mainScrollView)
-        self.mainScrollView.addSubview(mainContentView)
-        self.mainContentView.addSubview(locationStackView)
-        self.locationStackView.addArrangeSubViews(locationView)
-        self.locationView.addSubViews(myLocationLabel, cityLabel, weatherLabel, tempLabel, maxMinTempLabel)
-        
-        // navigationBar Setting
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationBar.barTintColor = .black
         navigationItem.title = "Weather"
@@ -113,65 +83,60 @@ extension MainViewController {
         navigationItem.searchController?.searchBar.placeholder = "Search for a city or airport"
         
         setupLayout()
-        
     }
     
     // 레이아웃 세팅
     private func setupLayout() {
-        mainScrollView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([mainScrollView.topAnchor.constraint(equalTo: self.view.topAnchor),
-                                     mainScrollView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
-                                     mainScrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
-                                     mainScrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)])
+        self.view.addSubview(mainScrollView)
+        self.mainScrollView.addSubview(mainContentView)
+        self.mainContentView.addSubview(locationStackView)
+        self.locationStackView.addArrangeSubViews(locationView)
+        self.locationView.addSubViews(myLocationLabel, cityLabel, weatherLabel, tempLabel, maxMinTempLabel)
         
-        mainContentView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([mainContentView.topAnchor.constraint(equalTo: mainScrollView.contentLayoutGuide.topAnchor),
-                                     mainContentView.bottomAnchor.constraint(equalTo: mainScrollView.contentLayoutGuide.bottomAnchor),
-                                     mainContentView.leadingAnchor.constraint(equalTo: mainScrollView.contentLayoutGuide.leadingAnchor),
-                                     mainContentView.trailingAnchor.constraint(equalTo: mainScrollView.contentLayoutGuide.trailingAnchor),
-                                     mainContentView.widthAnchor.constraint(equalTo: mainScrollView.widthAnchor)])
-        
-        locationStackView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([locationStackView.topAnchor.constraint(equalTo: self.mainContentView.topAnchor),
-                                     locationStackView.bottomAnchor.constraint(equalTo: self.mainContentView.bottomAnchor),
-                                     locationStackView.leadingAnchor.constraint(equalTo: self.mainContentView.leadingAnchor),
-                                     locationStackView.trailingAnchor.constraint(equalTo: self.mainContentView.trailingAnchor),
-                                     locationStackView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width)])
-        
-        locationView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([locationView.leadingAnchor.constraint(equalTo: self.locationStackView.leadingAnchor, constant: 20),
-                                     locationView.trailingAnchor.constraint(equalTo: self.locationStackView.trailingAnchor, constant: -50),
-                                     locationView.heightAnchor.constraint(equalToConstant: 117)])
-        
-        myLocationLabel.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([myLocationLabel.topAnchor.constraint(equalTo: self.locationView.topAnchor, constant: 10),
-                                     myLocationLabel.leadingAnchor.constraint(equalTo: self.locationView.leadingAnchor, constant: 16),
-                                     myLocationLabel.widthAnchor.constraint(equalToConstant: 96),
-                                     myLocationLabel.heightAnchor.constraint(equalToConstant: 30)])
-        
-        cityLabel.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([cityLabel.topAnchor.constraint(equalTo: self.myLocationLabel.bottomAnchor, constant: 0),
-                                     cityLabel.leadingAnchor.constraint(equalTo: self.locationView.leadingAnchor, constant: 16),
-                                     cityLabel.widthAnchor.constraint(equalToConstant: 60),
-                                     cityLabel.heightAnchor.constraint(equalToConstant: 20)])
-        
-        weatherLabel.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([weatherLabel.bottomAnchor.constraint(equalTo: self.locationView.bottomAnchor, constant: -11),
-                                     weatherLabel.leadingAnchor.constraint(equalTo: self.locationView.leadingAnchor, constant: 16),
-                                     weatherLabel.widthAnchor.constraint(equalToConstant: 30),
-                                     weatherLabel.heightAnchor.constraint(equalToConstant: 20)])
-        
-        tempLabel.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([tempLabel.topAnchor.constraint(equalTo: self.locationView.topAnchor, constant: 4),
-                                     tempLabel.trailingAnchor.constraint(equalTo: self.locationView.trailingAnchor, constant: -13),
-                                     tempLabel.widthAnchor.constraint(equalToConstant: 82),
-                                     tempLabel.heightAnchor.constraint(equalToConstant: 63)])
-        
-        maxMinTempLabel.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([maxMinTempLabel.bottomAnchor.constraint(equalTo: self.locationView.bottomAnchor, constant: -11),
-                                     maxMinTempLabel.trailingAnchor.constraint(equalTo: self.locationView.trailingAnchor, constant: -19),
-                                     maxMinTempLabel.widthAnchor.constraint(equalToConstant: 120),
-                                     maxMinTempLabel.heightAnchor.constraint(equalToConstant: 20)])
+        mainScrollView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        mainContentView.snp.makeConstraints { make in
+            make.top.bottom.centerX.width.equalToSuperview()
+        }
+        locationStackView.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.leading.trailing.equalToSuperview().inset(20)
+        }
+        locationView.snp.makeConstraints { make in
+            make.height.equalTo(117)
+            make.centerY.equalToSuperview()
+        }
+        myLocationLabel.snp.makeConstraints { make in
+            make.width.equalTo(200)
+            make.height.equalTo(30)
+            make.top.equalToSuperview().inset(10)
+            make.leading.equalToSuperview().inset(16)
+        }
+        cityLabel.snp.makeConstraints { make in
+            make.width.equalTo(60)
+            make.height.equalTo(20)
+            make.top.equalTo(myLocationLabel.snp.bottom).inset(0)
+            make.leading.equalToSuperview().inset(16)
+        }
+        weatherLabel.snp.makeConstraints { make in
+            make.width.equalTo(30)
+            make.height.equalTo(20)
+            make.leading.equalToSuperview().inset(16)
+            make.bottom.equalToSuperview().inset(11)
+        }
+        tempLabel.snp.makeConstraints { make in
+            make.width.equalTo(82)
+            make.height.equalTo(63)
+            make.top.equalToSuperview().inset(4)
+            make.trailing.equalToSuperview().inset(13)
+        }
+        maxMinTempLabel.snp.makeConstraints { make in
+            make.width.equalTo(120)
+            make.height.equalTo(20)
+            make.bottom.equalToSuperview().inset(11)
+            make.trailing.equalToSuperview().inset(13)
+        }
     }
     
     // GestureRecognizer 세팅
